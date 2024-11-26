edbabe0b50deb2370cda23602cf90f9318bb6784
PatrickRicardoFontini <161254473+PatrickRicardoFontini@users.noreply.github.com>
2024-11-26 10:39:54 -0300

    GetMapping
    Cliente

--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/ClienteController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.ecommerce.ecommerce.controller;
 
 import com.ecommerce.ecommerce.model.Cliente;
 import com.ecommerce.ecommerce.model.Endereco;
+import com.ecommerce.ecommerce.model.Produto;
 import com.ecommerce.ecommerce.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,39 +25,9 @@ public class ClienteController {
         List<Cliente> clientes = this.clienteRepository.findAll();
         return ResponseEntity.ok(clientes);
     }
-
-    // Endpoint para buscar um cliente por ID
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) {
-//        Optional<Cliente> cliente = clienteRepository.findById(id);
-//        return cliente.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    // Endpoint para criar um novo cliente
-//    @PostMapping
-//    public ResponseEntity<Cliente> criar(@RequestBody Cliente cliente) {
-//        Cliente clienteSalvo = clienteRepository.save(cliente);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
-//    }
-//
-//    // Endpoint para atualizar um cliente
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Cliente> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
-//        if (!clienteRepository.existsById(id)) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        cliente.setId(id); // Garantir que o ID seja atualizado corretamente
-//        Cliente clienteAtualizado = clienteRepository.save(cliente);
-//        return ResponseEntity.ok(clienteAtualizado);
-//    }
-//
-//    // Endpoint para deletar um cliente
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
-//        if (!clienteRepository.existsById(id)) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        clienteRepository.deleteById(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Integer id) {
+        return this.clienteRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Cliente não foi encontrado"));
+    }
 }
+
