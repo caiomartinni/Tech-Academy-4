83e6e35b387c672f1149ef95a6d0e8fb9011ce64
PatrickRicardoFontini <161254473+PatrickRicardoFontini@users.noreply.github.com>
2024-11-26 10:41:03 -0300

    GetMapping
    Categoria

--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/CategoriaController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.ecommerce.ecommerce.controller;
 
 import com.ecommerce.ecommerce.model.Categoria;
 import com.ecommerce.ecommerce.model.Cliente;
+import com.ecommerce.ecommerce.model.Produto;
 import com.ecommerce.ecommerce.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,35 +24,8 @@ public class CategoriaController {
         List<Categoria> categorias = this.categoriaRepository.findAll();
         return ResponseEntity.ok(categorias);
     }
-
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Categoria> buscarPorId(@PathVariable Integer id) {
-//        Optional<Categoria> categoria = categoriaRepository.findById(id);
-//        return categoria.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
-//        Categoria categoriaSalva = categoriaRepository.save(categoria);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Categoria> atualizar(@PathVariable Integer id, @RequestBody Categoria categoria) {
-//        if (!categoriaRepository.existsById(id)) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        categoria.setId(id);
-//        Categoria categoriaAtualizada = categoriaRepository.save(categoria);
-//        return ResponseEntity.ok(categoriaAtualizada);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
-//        if (!categoriaRepository.existsById(id)) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        categoriaRepository.deleteById(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @GetMapping("/{id}")
+    public Categoria findById(@PathVariable Integer id) {
+        return this.categoriaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Categoria não foi encontrado"));
+    }
 }
