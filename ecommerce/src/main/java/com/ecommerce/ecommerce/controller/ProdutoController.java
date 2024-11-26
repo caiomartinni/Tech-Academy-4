06b2f8debfad9da14abbae0d5b10560b2c6d43a0
PatrickRicardoFontini <161254473+PatrickRicardoFontini@users.noreply.github.com>
2024-11-26 10:54:12 -0300

    DeleteByID
    Produto

--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/ProdutoController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/ProdutoController.java
@@ -27,4 +27,13 @@ public class ProdutoController {
     public Produto findById(@PathVariable Integer id) {
         return this.produtoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Produto não foi encontrado"));
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        Optional<Produto>produtoOptional = produtoRepository.findById(id);
+        if (produtoOptional.isEmpty()){
+            return ResponseEntity.badRequest().body("Produto não encontrado com o ID fornecido.");
+        }
+        produtoRepository.deleteById(id);
+    return ResponseEntity.ok().body("Produdo deletado com sucesso!");
+    }
 }
