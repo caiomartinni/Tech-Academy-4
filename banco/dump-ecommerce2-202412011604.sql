eea5ef67f7f5ed7d7f8053495803c406728c0c38
caiomartinni <caio.martini@grupointegrado.br>
2024-12-01 16:05:41 -0300

    criação de trigger

---
+++ banco/dump-ecommerce2-202412011604.sql
@@ -0,0 +1,336 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecommerce2
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auditoria`
+--
+
+DROP TABLE IF EXISTS `auditoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auditoria` (
+  `id_auditoria` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `valor_atual` decimal(10,2) DEFAULT NULL,
+  `valor_novo` decimal(10,2) DEFAULT NULL,
+  `motivo` text DEFAULT NULL,
+  `data_mod` date DEFAULT NULL,
+  PRIMARY KEY (`id_auditoria`),
+  KEY `id_produto` (`id_produto`),
+  CONSTRAINT `auditoria_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auditoria`
+--
+
+LOCK TABLES `auditoria` WRITE;
+/*!40000 ALTER TABLE `auditoria` DISABLE KEYS */;
+INSERT INTO `auditoria` VALUES (1,4,719.99,699.99,'Alteração de preços','2024-12-01');
+/*!40000 ALTER TABLE `auditoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'string'),(2,'Processadores'),(3,'Memória RAM'),(4,'Armazenamento'),(5,'Fontes'),(6,'Gabinetes'),(7,'Placas Mãe'),(8,'Coolers'),(9,'Kit Upgrade'),(10,'teste');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cliente` varchar(100) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `data_nasc` date DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` int(11) DEFAULT NULL,
+  `id_endereco` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`),
+  KEY `id_endereco` (`id_endereco`),
+  CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id_endereco`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'string','string','2024-11-27','string',0,NULL),(2,'sdfsdfsdf','234234234',NULL,'sdfsdf',23423423,NULL),(3,'patrick','00000000',NULL,'string',0,NULL);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `endereco` (
+  `id_endereco` int(11) NOT NULL AUTO_INCREMENT,
+  `logr` varchar(100) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `cep` int(11) NOT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  PRIMARY KEY (`id_endereco`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereco`
+--
+
+LOCK TABLES `endereco` WRITE;
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flyway_schema_history`
+--
+
+DROP TABLE IF EXISTS `flyway_schema_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flyway_schema_history` (
+  `installed_rank` int(11) NOT NULL,
+  `version` varchar(50) DEFAULT NULL,
+  `description` varchar(200) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `script` varchar(1000) NOT NULL,
+  `checksum` int(11) DEFAULT NULL,
+  `installed_by` varchar(100) NOT NULL,
+  `installed_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `execution_time` int(11) NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  PRIMARY KEY (`installed_rank`),
+  KEY `flyway_schema_history_s_idx` (`success`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flyway_schema_history`
+--
+
+LOCK TABLES `flyway_schema_history` WRITE;
+/*!40000 ALTER TABLE `flyway_schema_history` DISABLE KEYS */;
+INSERT INTO `flyway_schema_history` VALUES (1,'1','create-database','SQL','V1__create-database.sql',535608882,'root','2024-11-26 00:26:31',625,1);
+/*!40000 ALTER TABLE `flyway_schema_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forma_pgto`
+--
+
+DROP TABLE IF EXISTS `forma_pgto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `forma_pgto` (
+  `id_forma_pgto` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_forma_pgto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forma_pgto`
+--
+
+LOCK TABLES `forma_pgto` WRITE;
+/*!40000 ALTER TABLE `forma_pgto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forma_pgto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_pedido`
+--
+
+DROP TABLE IF EXISTS `item_pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_pedido` (
+  `id_pedido` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  PRIMARY KEY (`id_pedido`,`id_produto`),
+  KEY `fk_produto` (`id_produto`),
+  CONSTRAINT `fk_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`),
+  CONSTRAINT `fk_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_pedido`
+--
+
+LOCK TABLES `item_pedido` WRITE;
+/*!40000 ALTER TABLE `item_pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagamento`
+--
+
+DROP TABLE IF EXISTS `pagamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pagamento` (
+  `id_pedido` int(11) NOT NULL,
+  `id_forma_pgto` int(11) NOT NULL,
+  `data_pgto` date NOT NULL,
+  PRIMARY KEY (`id_pedido`,`id_forma_pgto`,`data_pgto`),
+  KEY `fk_pagamento_forma_pgto` (`id_forma_pgto`),
+  CONSTRAINT `fk_pagamento_forma_pgto` FOREIGN KEY (`id_forma_pgto`) REFERENCES `forma_pgto` (`id_forma_pgto`),
+  CONSTRAINT `fk_pagamento_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagamento`
+--
+
+LOCK TABLES `pagamento` WRITE;
+/*!40000 ALTER TABLE `pagamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `data_pedido` date NOT NULL,
+  `total` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+INSERT INTO `pedido` VALUES (1,2,'2024-11-27',900.00),(2,2,'2024-11-27',4141.00),(3,2,'2024-11-27',1234.00),(4,2,'2024-11-27',1234.00),(5,1,'2024-11-28',5000.00);
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` varchar(100) NOT NULL,
+  `ds_produto` text NOT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id_produto`),
+  KEY `id_categoria` (`id_categoria`),
+  CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'ROCKZAO','string',0.00,1),(2,'Placa de Vídeo RX 6750XT Gaming XFX','a',2199.99,1),(3,'Placa de Vídeo RTX 4070 Super Gigabyte','a',4199.99,1),(4,'Placa de Vídeo RX 570 Phantom Gaming Elite ASRock','a',699.99,1),(5,'Placa de Vídeo RX 7600 GAMING OC 8G AMD Radeon Gigabyte','a',1799.99,1),(6,'Processador AMD Ryzen 7 5700X3D','a',1397.99,2),(7,'Processador Intel Core i5-12400F','a',669.99,2),(8,'Processador AMD Ryzen 7 7800X3D','a',2999.99,2),(9,'Processador Intel Core i3-10105','a',599.99,2),(10,'Processador Intel Core i9-14900KF','a',3599.99,2),(11,'Fonte Gamer Rise Mode Zeus_500W_White_PFC Ativo_Preto','a',219.99,5),(12,'Fonte Gamer Rise Mode Zeus 850W_Platinum_Full Modular_PFC Ativo','a',629.99,5),(13,'Fonte Corsair CX Series CX750_750W_80 Plus Bronze_Com Cabo','a',499.99,5),(14,'Fonte XPG Core Reactor_850W_80 Plus Gold_Modular_com cabo','a',629.99,5),(15,'Fonte MSI MAG A850GL, 850W, 80 Plus Gold, Modular, PFC Ativo','a',679.99,5),(16,'Memória RAM Kingston Fury Beast, 16GB, 3200MHz, DDR4','a',273.99,3),(17,'Memória RAM Gamer Husky Gaming Avalanche, 8GB, 3200MHz, DDR4','a',139.99,3),(18,'Memória RAM Kingston Fury Beast, RGB, 16GB, 3200MHz, DDR4','a',299.99,3),(19,'Memória RAM Rise Mode Z, 8GB, 3200MHz, DDR4','a',119.99,3),(20,'Memória RAM XPG Spectrix D35G, RGB, 16GB, 3200MHz, DDR4','a',264.99,3),(21,'HD Seagate IronWolf NAS, 8TB, SATA','a',1309.99,4),(22,'HD Externo Portátil 2TB Seagate, USB 3.0','a',489.99,4),(23,'SSD Kingston A400, 480 GB, SATA III, 2.5\"','a',239.99,4),(24,'SSD Kingston NV3, 500 GB','a',339.99,4),(25,'SSD Kingston Fury Renegade, 1 TB, M.2','a',599.99,4),(26,'Placa Mãe Asus TUF GAMING A520M-PLUS II, AMD AM4','a',599.99,7),(27,'Placa Mãe Gigabyte B550M Aorus Elite (rev. 1.3), AMD AM4, Micro ATX, DDR4','a',829.99,7),(28,'Placa Mãe MSI A520M-A PRO, AMD AM4, mATX, DDR4','a',399.99,7),(29,'Placa Mãe ASRock B450M Steel Legend, AMD AM4, mATX, DDR4','a',769.99,7),(30,'Placa Mãe Gigabyte X670e Aorus Xtreme, DDR5, E-ATX, AM5','a',6581.52,7),(31,'Gabinete Gamer Corsair 4000D Airflow, Mid-Tower','a',599.90,6),(32,'Gabinete Gamer Rise Mode Glass 06X, Mid Tower','a',229.90,6),(33,'Gabinete Gamer Redragon Superion, Mid Tower','a',289.90,6),(34,'Gabinete Gamer Sharkoon TG4, RGB, Mid Tower','a',329.90,6),(35,'Gabinete Corsair iCUE Link 6500X, Mid Tower','a',2329.90,6),(36,'RiseMode Frost 360mm','string',400.00,8),(38,'string','string',212.00,1),(39,'string','string',212.00,1),(40,'praca de video','aaaaa',10000.00,1);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER trg_audit_preco_produto
+AFTER UPDATE ON produto -- Corrigido o uso de ON
+FOR EACH ROW
+BEGIN
+    -- Verifica se o preço foi alterado
+    IF OLD.preco <> NEW.preco THEN
+        INSERT INTO auditoria (id_produto, valor_atual, valor_novo, motivo, data_mod)
+        VALUES
+        (NEW.id_produto, OLD.preco, NEW.preco, 'Alteração de preços', NOW());
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'ecommerce2'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-01 16:04:58
